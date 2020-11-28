d28d705fd08c08fb8b545b9fb3ff25081bdf4282
Javier Barceló Pérez <javibarpe@hotmail.com>
2020-11-28 02:11:37 +0100

    Minor changes on LoginWindow and enchanced RegisterWindow
     - Added mostrarVentana() in both classes
     - RegisterWindow no longer extends JFrame but includes one as a private atributte
     - Changed password text fields to password fields
     - Implemented an error if passwords don't match
     - Button 'Cancel' closes the RegisterWindow and returns to the LoginWindow
     - RegisterWindow no longer disposes on close but exits on close

--- AppMusic/src/umu/tds/ui/RegisterWindow.java
+++ AppMusic/src/umu/tds/ui/RegisterWindow.java
@@ -24,8 +24,11 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JPasswordField;
 
-public class RegisterWindow extends JFrame {
+public class RegisterWindow{
+	private JFrame frame;
 	private JTextField nameTextField;
 	private JTextField lastNameTextField;
 	private JTextField emailTextField;
@@ -35,6 +38,7 @@ public class RegisterWindow extends JFrame {
 	private JPanel panel;
 	private JButton btnRegister;
 	private JButton btnCancel;
+	private JLabel lblRegistrationData;
 
 	/**
 	 * Launch the application.
@@ -44,7 +48,7 @@ public class RegisterWindow extends JFrame {
 			public void run() {
 				try {
 					RegisterWindow frame = new RegisterWindow(null);
-					frame.setVisible(true);
+					frame.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -52,29 +56,44 @@ public class RegisterWindow extends JFrame {
 		});
 	}
 
+	
+	public void mostrarVentana() {
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+	
 	/**
 	 * Create the frame.
 	 */
 	public RegisterWindow(JFrame invoker) {
 		// addWindowListener();
-		
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 650, 292);
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 489, 398);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 100, 216, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		getContentPane().setLayout(gridBagLayout);
-		((JComponent) getContentPane()).setBorder(new EmptyBorder(20, 20, 20, 20));
+		gridBagLayout.columnWidths = new int[] {30, 100, 50, 0, 50, 0};
+		gridBagLayout.rowHeights = new int[] {0, 0, 30, 0, 0, 0, 0, 0, 0, 30, 30, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 1.0, 0.0};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		((JComponent) frame.getContentPane()).setBorder(new EmptyBorder(20, 20, 20, 20));
 		
-		JLabel lblNombre = new JLabel("First name:");
+		lblRegistrationData = new JLabel("Registration data:");
+		lblRegistrationData.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		GridBagConstraints gbc_lblRegistrationData = new GridBagConstraints();
+		gbc_lblRegistrationData.gridwidth = 4;
+		gbc_lblRegistrationData.insets = new Insets(0, 0, 5, 5);
+		gbc_lblRegistrationData.gridx = 1;
+		gbc_lblRegistrationData.gridy = 1;
+		frame.getContentPane().add(lblRegistrationData, gbc_lblRegistrationData);
+		
+		JLabel lblNombre = new JLabel("Name:");
 		GridBagConstraints gbc_lblNombre = new GridBagConstraints();
 		gbc_lblNombre.anchor = GridBagConstraints.EAST;
 		gbc_lblNombre.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNombre.gridx = 1;
-		gbc_lblNombre.gridy = 1;
-		getContentPane().add(lblNombre, gbc_lblNombre);
+		gbc_lblNombre.gridy = 3;
+		frame.getContentPane().add(lblNombre, gbc_lblNombre);
 		
 		nameTextField = new JTextField();
 		GridBagConstraints gbc_nameTextField = new GridBagConstraints();
@@ -82,17 +101,17 @@ public class RegisterWindow extends JFrame {
 		gbc_nameTextField.insets = new Insets(0, 0, 5, 5);
 		gbc_nameTextField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_nameTextField.gridx = 2;
-		gbc_nameTextField.gridy = 1;
-		getContentPane().add(nameTextField, gbc_nameTextField);
+		gbc_nameTextField.gridy = 3;
+		frame.getContentPane().add(nameTextField, gbc_nameTextField);
 		nameTextField.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Last name(s):");
+		JLabel lblNewLabel = new JLabel("Surname:");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.gridx = 1;
-		gbc_lblNewLabel.gridy = 2;
-		getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+		gbc_lblNewLabel.gridy = 4;
+		frame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
 		
 		lastNameTextField = new JTextField();
 		GridBagConstraints gbc_lastNameTextField = new GridBagConstraints();
@@ -100,8 +119,8 @@ public class RegisterWindow extends JFrame {
 		gbc_lastNameTextField.insets = new Insets(0, 0, 5, 5);
 		gbc_lastNameTextField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_lastNameTextField.gridx = 2;
-		gbc_lastNameTextField.gridy = 2;
-		getContentPane().add(lastNameTextField, gbc_lastNameTextField);
+		gbc_lastNameTextField.gridy = 4;
+		frame.getContentPane().add(lastNameTextField, gbc_lastNameTextField);
 		lastNameTextField.setColumns(10);
 		
 		JLabel lblBirthDate = new JLabel("Birth Date:");
@@ -109,8 +128,8 @@ public class RegisterWindow extends JFrame {
 		gbc_lblBirthDate.anchor = GridBagConstraints.EAST;
 		gbc_lblBirthDate.insets = new Insets(0, 0, 5, 5);
 		gbc_lblBirthDate.gridx = 1;
-		gbc_lblBirthDate.gridy = 3;
-		getContentPane().add(lblBirthDate, gbc_lblBirthDate);
+		gbc_lblBirthDate.gridy = 5;
+		frame.getContentPane().add(lblBirthDate, gbc_lblBirthDate);
 		
 		JDateChooser birthDateChooser = new JDateChooser(null, null, null, new JSpinnerDateEditor());
 		GridBagConstraints gbc_birthDateChooser = new GridBagConstraints();
@@ -118,8 +137,8 @@ public class RegisterWindow extends JFrame {
 		gbc_birthDateChooser.insets = new Insets(0, 0, 5, 5);
 		gbc_birthDateChooser.fill = GridBagConstraints.HORIZONTAL;
 		gbc_birthDateChooser.gridx = 2;
-		gbc_birthDateChooser.gridy = 3;
-		getContentPane().add(birthDateChooser, gbc_birthDateChooser);
+		gbc_birthDateChooser.gridy = 5;
+		frame.getContentPane().add(birthDateChooser, gbc_birthDateChooser);
 		
 		
 		JLabel lblEmail = new JLabel("Email:");
@@ -127,8 +146,8 @@ public class RegisterWindow extends JFrame {
 		gbc_lblEmail.anchor = GridBagConstraints.EAST;
 		gbc_lblEmail.insets = new Insets(0, 0, 5, 5);
 		gbc_lblEmail.gridx = 1;
-		gbc_lblEmail.gridy = 4;
-		getContentPane().add(lblEmail, gbc_lblEmail);
+		gbc_lblEmail.gridy = 6;
+		frame.getContentPane().add(lblEmail, gbc_lblEmail);
 		
 		emailTextField = new JTextField();
 		GridBagConstraints gbc_emailTextField = new GridBagConstraints();
@@ -136,8 +155,8 @@ public class RegisterWindow extends JFrame {
 		gbc_emailTextField.insets = new Insets(0, 0, 5, 5);
 		gbc_emailTextField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_emailTextField.gridx = 2;
-		gbc_emailTextField.gridy = 4;
-		getContentPane().add(emailTextField, gbc_emailTextField);
+		gbc_emailTextField.gridy = 6;
+		frame.getContentPane().add(emailTextField, gbc_emailTextField);
 		emailTextField.setColumns(10);
 		
 		JLabel lblUsername = new JLabel("Username:");
@@ -145,8 +164,8 @@ public class RegisterWindow extends JFrame {
 		gbc_lblUsername.anchor = GridBagConstraints.EAST;
 		gbc_lblUsername.insets = new Insets(0, 0, 5, 5);
 		gbc_lblUsername.gridx = 1;
-		gbc_lblUsername.gridy = 5;
-		getContentPane().add(lblUsername, gbc_lblUsername);
+		gbc_lblUsername.gridy = 7;
+		frame.getContentPane().add(lblUsername, gbc_lblUsername);
 		
 		usernameTextField = new JTextField();
 		GridBagConstraints gbc_usernameTextField = new GridBagConstraints();
@@ -154,8 +173,8 @@ public class RegisterWindow extends JFrame {
 		gbc_usernameTextField.insets = new Insets(0, 0, 5, 5);
 		gbc_usernameTextField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_usernameTextField.gridx = 2;
-		gbc_usernameTextField.gridy = 5;
-		getContentPane().add(usernameTextField, gbc_usernameTextField);
+		gbc_usernameTextField.gridy = 7;
+		frame.getContentPane().add(usernameTextField, gbc_usernameTextField);
 		usernameTextField.setColumns(10);
 		
 		JLabel lblPassword = new JLabel("Password:");
@@ -163,34 +182,35 @@ public class RegisterWindow extends JFrame {
 		gbc_lblPassword.anchor = GridBagConstraints.EAST;
 		gbc_lblPassword.insets = new Insets(0, 0, 5, 5);
 		gbc_lblPassword.gridx = 1;
-		gbc_lblPassword.gridy = 6;
-		getContentPane().add(lblPassword, gbc_lblPassword);
+		gbc_lblPassword.gridy = 8;
+		frame.getContentPane().add(lblPassword, gbc_lblPassword);
 		
-		passwordTextField = new JTextField();
+		passwordTextField = new JPasswordField();
 		GridBagConstraints gbc_passwordTextField = new GridBagConstraints();
 		gbc_passwordTextField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_passwordTextField.insets = new Insets(0, 0, 5, 5);
 		gbc_passwordTextField.gridx = 2;
-		gbc_passwordTextField.gridy = 6;
-		getContentPane().add(passwordTextField, gbc_passwordTextField);
+		gbc_passwordTextField.gridy = 8;
+		frame.getContentPane().add(passwordTextField, gbc_passwordTextField);
 		passwordTextField.setColumns(10);
 		
-		JLabel lblRepeatPassword = new JLabel("Repeat password:");
+		JLabel lblRepeatPassword = new JLabel("Repeat:");
 		GridBagConstraints gbc_lblRepeatPassword = new GridBagConstraints();
 		gbc_lblRepeatPassword.anchor = GridBagConstraints.EAST;
 		gbc_lblRepeatPassword.insets = new Insets(0, 0, 5, 5);
 		gbc_lblRepeatPassword.gridx = 3;
-		gbc_lblRepeatPassword.gridy = 6;
-		getContentPane().add(lblRepeatPassword, gbc_lblRepeatPassword);
+		gbc_lblRepeatPassword.gridy = 8;
+		frame.getContentPane().add(lblRepeatPassword, gbc_lblRepeatPassword);
 		
-		rPasswordTextField = new JTextField();
+		rPasswordTextField = new JPasswordField();
 		GridBagConstraints gbc_rPasswordTextField = new GridBagConstraints();
 		gbc_rPasswordTextField.insets = new Insets(0, 0, 5, 5);
 		gbc_rPasswordTextField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_rPasswordTextField.gridx = 4;
-		gbc_rPasswordTextField.gridy = 6;
-		getContentPane().add(rPasswordTextField, gbc_rPasswordTextField);
+		gbc_rPasswordTextField.gridy = 8;
+		frame.getContentPane().add(rPasswordTextField, gbc_rPasswordTextField);
 		rPasswordTextField.setColumns(10);
+
 		
 		panel = new JPanel();
 		GridBagConstraints gbc_panel = new GridBagConstraints();
@@ -198,8 +218,8 @@ public class RegisterWindow extends JFrame {
 		gbc_panel.insets = new Insets(0, 0, 0, 5);
 		gbc_panel.fill = GridBagConstraints.BOTH;
 		gbc_panel.gridx = 1;
-		gbc_panel.gridy = 9;
-		getContentPane().add(panel, gbc_panel);
+		gbc_panel.gridy = 11;
+		frame.getContentPane().add(panel, gbc_panel);
 		
 		btnRegister = new JButton("Register");
 		btnRegister.addActionListener(new ActionListener() {
@@ -212,20 +232,31 @@ public class RegisterWindow extends JFrame {
 				String email = txtMail.getText();
 				String firstName = txtFirstName.getText();
 				String lastName = txtLastName.getText();
+				*/
 				// LocalDate birthDate = LocalDate.of()
-				if(!pass1.equals(pass2)) {
-					JOptionPane.showInternalMessageDialog(btnRegister, "Register error");
+				
+				if(!passwordTextField.getText().equals(rPasswordTextField.getText())) {
+					JOptionPane.showMessageDialog(frame, "Not matching passwords!", "Password Error", JOptionPane.ERROR_MESSAGE);
 				}
-				else {
+				
+					
 					//AppMusic.getInstance().registerUser(username, pass1, firstName, lastName, email, birthDate);
 					// Quizás ahora cerrar ventana registro y pasar a main.
-				}
-				*/
+				
+				
 			}
 		});
 		panel.add(btnRegister);
 		
 		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				LoginWindow login = new LoginWindow();
+				login.mostrarVentana();
+				frame.dispose();
+				
+			}
+		});
 		panel.add(btnCancel);
 	}
 	
