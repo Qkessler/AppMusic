8c181dc2bfdeee76e7feb0a6ab7e1271e3d1d3ab
Javier Barceló Pérez <javibarpe@hotmail.com>
2020-11-29 11:35:15 +0100

    Added maven.bat for easier functionallity in windows
    No uso linux sry not sry

---
+++ AppMusicMaven/maven.bat
@@ -0,0 +1,5 @@
+SET M2_HOME=D:\Programas\Maven
+SET M2=D:\Programas\Maven\bin
+SET PATH=%M2%;%PATH%
+SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0_231
+SET M2_REPO=C:\Users\javib\OneDrive\UNIVERSIDAD\3º - CURSO 2020-2021\TDS\maven
